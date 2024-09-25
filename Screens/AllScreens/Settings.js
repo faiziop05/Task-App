3402b00feb624d4cdb7c86aa31f8ecf18cb6e459
Faizan Hanif <faizanst123@gmail.com>
2024-09-25 14:14:20 +0500

    second  commit

--- Screens/AllScreens/Settings.js
+++ Screens/AllScreens/Settings.js
@@ -1,16 +1,15 @@
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import React from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { setFalse, setUser } from "../../redux/loginSlice";
 import { useDispatch } from "react-redux";
 import { setTodoList } from "../../redux/TodoSlice";
+import { screenHeight, screenWidth } from "../../Dimentions";
 
 const Settings = () => {
   const dispatch = useDispatch();
   const handleLogout = async () => {
     await AsyncStorage.clear();
-    // await AsyncStorage.setItem("isLoggedIn", "false");
-    // await AsyncStorage.removeItem("TodoList");
     dispatch(setFalse());
     dispatch(setTodoList([]));
     dispatch(
@@ -21,12 +20,33 @@ const Settings = () => {
     );
   };
   return (
-    <View>
-      <Button onPress={handleLogout} title="Logout" />
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.logoutbtn} onPress={handleLogout}>
+        <Text style={styles.logoutbtnText}>Logout</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 export default Settings;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container:{
+    backgroundColor:'#fff',
+    height:screenHeight,
+    alignItems:"center"
+  },
+  logoutbtn:{
+    backgroundColor:'red',
+    borderRadius:20,
+    width:screenWidth-40,
+    height:50,
+    alignItems:"center",
+    justifyContent:"center",
+    marginVertical:20
+    
+  },
+  logoutbtnText:{
+    color:'white'
+  }
+});
